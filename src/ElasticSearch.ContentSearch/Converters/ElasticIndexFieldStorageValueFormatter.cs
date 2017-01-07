7b0d356cc9c2662dd60c3e7ff69c3c579d431f05
Søren Kruse <krusenen@gmail.com>
2017-01-08 00:00:45 +0100

    Update to Sitecore 8.2 (.NET 4.5.2) and add a class

---
+++ src/ElasticSearch.ContentSearch/Converters/ElasticIndexFieldStorageValueFormatter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.ContentSearch.Converters;
+
+namespace ElasticSearch.ContentSearch.Converters
+{
+    public class ElasticIndexFieldStorageValueFormatter : IndexFieldStorageValueFormatter
+    {
+        public override object FormatValueForIndexStorage(object value, string fieldName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/ElasticSearch.ContentSearch/Linq/ElasticQuery.cs
+++ src/ElasticSearch.ContentSearch/Linq/ElasticQuery.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using Nest;
 using Sitecore.ContentSearch.Linq.Common;
 using Sitecore.ContentSearch.Linq.Methods;
+using IQuery = Sitecore.ContentSearch.Linq.Common.IQuery;
 
 namespace ElasticSearch.ContentSearch.Linq
 {
     public class ElasticQuery : IQuery
     {
+        public QueryContainer Query { get; protected set; }
+        public QueryContainer Filter { get; protected set; }
         public List<QueryMethod> Methods { get; }
         public List<IFieldQueryTranslator> VirtualFieldProcessors { get; }
         public List<FacetQuery> FacetQueries { get; }
         public List<IExecutionContext> ExecutionContexts { get; }
+
+        public ElasticQuery(QueryContainer query, QueryContainer filter, IEnumerable<QueryMethod> methods, IEnumerable<IFieldQueryTranslator> virtualFieldProcessors, IEnumerable<FacetQuery> facetQueries, IEnumerable<IExecutionContext> executionContexts)
+        {
+            Query = query;
+            Filter = filter;
+            Methods = methods.ToList();
+            VirtualFieldProcessors = virtualFieldProcessors.ToList();
+            FacetQueries = facetQueries.ToList();
+            ExecutionContexts = executionContexts.ToList();
+        }
 
         public void WriteTo(TextWriter writer)
         {
